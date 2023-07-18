398448af03e6ea4aae2acd04574928133e970a84
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-07-17 19:40:08 -0600

    handlebars and js

---
+++ public/js/comments.js
@@ -0,0 +1,25 @@
+const commentFormHandler = async function (event) {
+	event.preventDefault();
+
+	const blog_id = document.querySelector('.new-comment-form').dataset.blogid;
+
+	const comment_description = document.querySelector('#comment_description').value.trim();
+
+	if (comment_description) {
+		await fetch('/api/comments', {
+			method: 'POST',
+			body: JSON.stringify({
+				blog_id,
+				comment_description,
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		document.location.reload();
+	}
+};
+
+document
+	.querySelector('.new-comment-form')
+	.addEventListener('submit', commentFormHandler);
