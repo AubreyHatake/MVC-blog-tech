d58c745fb353be7351190db95ad902b6c13a407b
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-07-18 19:03:15 -0600

    adding username to User models

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
 		const blogData = await Blog.findAll({
 			include: [{
 				model: User,
-				attributes: ['name'],
+				attributes: ['username'],
 			},],
 		});
 
@@ -30,7 +30,7 @@ router.get('/blog/:id', async (req, res) => {
 			include: [
 				{
 					model: User,
-					attributes: ['name'],
+					attributes: ['username'],
 				}, {
 					model: Comment,
 					include: [
